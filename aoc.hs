0741f0f032bdc2d4d9a7a1e46618e3683d6c7d5e
Brian Kubisiak <brian@kubisiak.com>
2021-12-01 19:04:39 -0800

    aoc: add hunit tests
    The main function will just run regression tests over each completed
    day.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- aoc.hs
+++ aoc.hs
@@ -1,3 +1,24 @@
+import qualified System.IO as IO
+import Test.HUnit
+
 import qualified Input
 
 import qualified Y2021
+
+testDay :: (Eq b, Show b, Read b) => (FilePath -> IO b) -> FilePath -> IO ()
+testDay f day = do
+  let vectors = [day ++ "-actual.txt", day ++ "-sample.txt"]
+  actual <- mapM f ["input/" ++ vector | vector <- vectors]
+  expected <- mapM IO.readFile ["output/" ++ vector | vector <- vectors]
+  assertEqual day actual $ map read expected
+
+allTests :: Test
+allTests = TestList [TestLabel day $ TestCase (testDay f day) |
+                     (f, day) <- [ (Y2021.d01a, "2021-01a")
+                                 , (Y2021.d01b, "2021-01b")
+                                 ]]
+
+main :: IO ()
+main = do
+  testResults <- runTestTT allTests
+  print testResults
